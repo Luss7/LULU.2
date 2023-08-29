08e89f918a736f75c0a73d8869b49f8711e43310
Lucie <44549123+Luss7@users.noreply.github.com>
2023-08-29 11:17:34 +0000

    NavBar

---
+++ src/components/Settings.tsx
@@ -0,0 +1,13 @@
+// Settings.tsx
+import React from 'react';
+
+function Settings() {
+  return (
+    <div>
+      <h2>Page des réglages</h2>
+      {/* Contenu de la page des réglages */}
+    </div>
+  );
+}
+
+export default Settings;
